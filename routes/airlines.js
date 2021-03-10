424909f887a8326d2db07d217311367852650418
salman-23 <66154732+salman-23@users.noreply.github.com>
2021-03-10 07:51:47 +0300

    flight update

--- routes/airlines.js
+++ routes/airlines.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 const {
   signup,
   signin,
@@ -8,40 +8,41 @@ const {
   airlineList,
   airlineDetail,
   flightCreate,
+  flightUpdate,
 } = require("../controllers/airlineControllers");
 const upload = require("../middleware/multer");
 
-
-router.param('airlineId', async (req, res, next, airlineId) => {
-	const foundAirline = await fetchAirline(airlineId, next);
-	if (foundAirline) {
-		req.airline = foundAirline;
-		next();
-	} else {
-		next({
-			status: 404,
-			message: 'Airline Not Found',
-		});
-	}
+router.param("airlineId", async (req, res, next, airlineId) => {
+  const foundAirline = await fetchAirline(airlineId, next);
+  if (foundAirline) {
+    req.airline = foundAirline;
+    next();
+  } else {
+    next({
+      status: 404,
+      message: "Airline Not Found",
+    });
+  }
 });
 
-router.post('/airlinesignup', upload.single('picture'), signup);
+router.post("/airlinesignup", upload.single("picture"), signup);
 
 router.post(
-	'/airlinesignin',
-	passport.authenticate('airline', { session: false }),
-	signin
+  "/airlinesignin",
+  passport.authenticate("airline", { session: false }),
+  signin
 );
 
 router.get("/", airlineList);
 
 router.get("/:airlineId", airlineDetail);
 
+router.put("/:airlineId/flights/:flightId", flightUpdate);
 
 router.post(
-	'/:airlineId/flights',
-	passport.authenticate('jwt-airline', { session: false }),
-	flightCreate
+  "/:airlineId/flights",
+  passport.authenticate("airline", { session: false }),
+  flightCreate
 );
 
 module.exports = router;
