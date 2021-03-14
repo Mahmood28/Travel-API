099e92665936bf1261dd7777ac63bc30c4c62eda
Mahmood28 <mahmoodalwatani@gmail.com>
2021-03-14 17:50:57 +0300

    booking controller.

--- controllers/bookingControllers.js
+++ controllers/bookingControllers.js
@@ -1,19 +1,37 @@
-const { Booking } = require('../db/models');
+const { Op } = require("sequelize");
+const { Booking, TravelClassCapacity } = require("../db/models");
 
 exports.bookingCreate = async (req, res, next) => {
-	try {
-		const newBook = await Booking.bulkCreate(req.body);
-		res.status(201).json(newBook);
-	} catch (error) {
-		next(error);
-	}
+  try {
+    const { flights, passangers, travelClassId } = req.body;
+    const newBookings = await Booking.bulkCreate(passangers);
+    newBookings.forEach(async (booking) => await booking.addFlights(flights));
+    const foundFlights = await TravelClassCapacity.findAll({
+      where: {
+        flightId: {
+          [Op.or]: flights,
+        },
+        travelClassId,
+      },
+    });
+    foundFlights.forEach(
+      async (flight) =>
+        await flight.update({
+          ...flight,
+          vacancy: flight.vacancy - passangers.length,
+        })
+    );
+    res.status(201).json(newBookings.map((booking) => ({ booking, flights })));
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.bookingList = async (req, res, next) => {
-	try {
-		const booking = await Booking.findAll();
-		res.json(booking);
-	} catch (error) {
-		next(error);
-	}
+  try {
+    const booking = await Booking.findAll();
+    res.json(booking);
+  } catch (error) {
+    next(error);
+  }
 };
--- db/models/Booking.js
+++ db/models/Booking.js
@@ -1,30 +1,30 @@
 module.exports = (sequelize, DataTypes) => {
-	const Booking = sequelize.define(
-		'Booking',
-		{
-			firstName: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			lastName: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			passport: {
-				type: DataTypes.STRING,
-				allowNull: true,
-			},
-			email: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			birthday: {
-				type: DataTypes.DATEONLY,
-				allowNull: true,
-			},
-		},
-		{ timestamps: false }
-	);
+  const Booking = sequelize.define(
+    "Booking",
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      passport: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      birthDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: true,
+      },
+    },
+    { timestamps: false }
+  );
 
-	return Booking;
+  return Booking;
 };
