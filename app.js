2fe3cd89b5c48e1f120124c6651b2bec9a83f497
salman-23 <66154732+salman-23@users.noreply.github.com>
2021-03-14 11:19:32 +0300

    edited create flight

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const db = require('./db/models');
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const userRoutes = require('./routes/users');
-const airlinesRoutes = require('./routes/airlines');
-const flightRoutes = require('./routes/flights');
+const db = require("./db/models");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const userRoutes = require("./routes/users");
+const airlinesRoutes = require("./routes/airlines");
+const flightRoutes = require("./routes/flights");
 // const { jwtStrategy } = require("./middleware/passport");
-const destinationRoutes = require('./routes/destinations');
-const bookingRoutes = require('./routes/booking');
-const passport = require('passport');
-require('./middleware/passport')(passport);
+const destinationRoutes = require("./routes/destinations");
+const bookingRoutes = require("./routes/booking");
+const passport = require("passport");
+require("./middleware/passport")(passport);
 
 const app = express();
 
@@ -20,29 +20,29 @@ app.use(passport.initialize());
 // passport.use(jwtStrategy);
 // app.use(airlinesRoutes);
 
-app.use('/flights', flightRoutes);
-app.use('/booking', bookingRoutes);
-app.use('/airlines', airlinesRoutes);
-app.use('/destinations', destinationRoutes);
-app.use('/user', userRoutes);
+app.use("/flights", flightRoutes);
+app.use("/booking", bookingRoutes);
+app.use("/airlines", airlinesRoutes);
+app.use("/destinations", destinationRoutes);
+app.use("/user", userRoutes);
 
-app.use('/media', express.static(path.join(__dirname, 'media')));
+app.use("/media", express.static(path.join(__dirname, "media")));
 
 app.use((req, res, next) => {
-	const error = {
-		status: 404,
-		message: 'Path Not Found!',
-	};
-	next(error);
+  const error = {
+    status: 404,
+    message: "Path Not Found!",
+  };
+  next(error);
 });
 
 app.use((err, req, res, next) => {
-	res
-		.status(err.status || 500)
-		.json({ message: err.message || 'Internal Server Error' });
+  res
+    .status(err.status || 500)
+    .json({ message: err.message || "Internal Server Error" });
 });
 
-//db.sequelize.sync();
+// db.sequelize.sync();
 db.sequelize.sync({ alter: true });
-//db.sequelize.sync({ force: true });
+// db.sequelize.sync({ force: true });
 app.listen(8000);
--- controllers/airlineControllers.js
+++ controllers/airlineControllers.js
@@ -1,121 +1,140 @@
-const bcrypt = require('bcrypt');
-const upload = require('../middleware/multer');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const upload = require("../middleware/multer");
+const jwt = require("jsonwebtoken");
 //models
-const { Airline, Flight } = require('../db/models');
+const { Airline, Flight, Destination } = require("../db/models");
 //keys
-const { JWT_SECRET, JWT_EXPIRATION_MS } = require('../config/keys');
+const { JWT_SECRET, JWT_EXPIRATION_MS } = require("../config/keys");
 
 exports.signup = async (req, res, next) => {
-	const { password } = req.body;
-	const saltRounds = 10;
-	try {
-		console.log(req.body);
-		if (req.file) {
-			req.body.logo = `http://${req.get('host')}/media/${req.file.filename}`;
-		}
-		const hashedPassword = await bcrypt.hash(password, saltRounds);
-		req.body.password = hashedPassword;
-		const newAirline = await Airline.create(req.body);
-		const payload = {
-			id: newAirline.id,
-			nameOfAirline: newAirline.nameOfAirline,
-			username: newAirline.username,
-			email: newAirline.email,
-			exp: Date.now() + JWT_EXPIRATION_MS,
-		};
-		const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
-		res.status(201).json({ token });
-	} catch (error) {
-		next(error);
-	}
+  const { password } = req.body;
+  const saltRounds = 10;
+  try {
+    console.log(req.body);
+    if (req.file) {
+      req.body.logo = `http://${req.get("host")}/media/${req.file.filename}`;
+    }
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    req.body.password = hashedPassword;
+    const newAirline = await Airline.create(req.body);
+    const payload = {
+      id: newAirline.id,
+      nameOfAirline: newAirline.nameOfAirline,
+      username: newAirline.username,
+      email: newAirline.email,
+      // isAirline: newAirline.isAirline,
+      exp: Date.now() + JWT_EXPIRATION_MS,
+    };
+    const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
+    res.status(201).json({ token });
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.signin = (req, res) => {
-	const { user } = req;
-	const payload = {
-		id: user.id,
-		username: user.username,
-		exp: Date.now() + parseInt(JWT_EXPIRATION_MS),
-	};
+  const { user } = req;
+  const payload = {
+    id: user.id,
+    username: user.username,
+    // isAirline: user.isAirline,
+    exp: Date.now() + parseInt(JWT_EXPIRATION_MS),
+  };
 
-	const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
-	res.json({ token });
+  const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
+  res.json({ token });
 };
 
 exports.fetchAirline = async (airlineId, next) => {
-	try {
-		const foundAirline = await Airline.findByPk(airlineId);
-		return foundAirline;
-	} catch (error) {
-		next(error);
-	}
+  try {
+    const foundAirline = await Airline.findByPk(airlineId);
+    return foundAirline;
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.airlineList = async (req, res, next) => {
-	try {
-		const airlines = await Airline.findAll({
-			include: {
-				model: Flight,
-				as: 'flights',
-				attributes: {
-					exclude: ['createdAt', 'updatedAt'],
-				},
-			},
-		});
+  try {
+    const airlines = await Airline.findAll({
+      include: {
+        model: Flight,
+        as: "flights",
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
+      },
+    });
 
-		res.json(airlines);
-	} catch (error) {
-		next(error);
-	}
+    res.json(airlines);
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.airlineDetail = async (req, res, next) => {
-	res.json(req.airline);
+  res.json(req.airline);
 };
 
 exports.flightCreate = async (req, res, next) => {
-	try {
-		const {
-			arrivalAirport,
-			departureAirport,
-			departureTime,
-			arrivalTime,
-		} = req.body;
-		req.body.airlineId = req.airline.id;
+  try {
+    const {
+      // arrivalAirport,
+      // departureAirport,
+      destinationId,
+      departureTime,
+      arrivalTime,
+      originId,
+    } = req.body;
+    req.body.airlineId = req.airline.id;
     // console.log(req.body);
-    const newFlight = await Flight.bulkCreate([
-      req.body,
+    const newFlight = await Flight.bulkCreate(
+      [
+        req.body,
+        {
+          ...req.body,
+          name: req.body.name + 1324,
+          destinationId: originId,
+          originId: destinationId,
+          departureTime: +arrivalTime + 0.5,
+          arrivalTime: 2 * +arrivalTime - +departureTime + 0.5,
+        },
+      ],
       {
-        ...req.body,
-        name: req.body.name + 1324,
-        arrivalAirport: departureAirport,
-        departureAirport: arrivalAirport,
-        departureTime: +arrivalTime + 0.5,
-        arrivalTime: 2 * +arrivalTime - +departureTime + 0.5,
-      },
-    ]);
+        include: [
+          {
+            model: Destination,
+            as: "destination",
+            attributes: ["airport", "code", "country", "city"],
+          },
+          {
+            model: Destination,
+            as: "origin",
+            attributes: ["airport", "code", "country", "city"],
+          },
+        ],
+      }
+    );
     res.status(201).json(newFlight);
   } catch (error) {
     console.log(error);
 
     next(error);
   }
-
 };
 
 exports.flightUpdate = async (req, res) => {
-	const { flightId } = req.params;
-	try {
-		const foundFlight = await Flight.findByPk(flightId);
-		if (foundFlight) {
-			await foundFlight.update(req.body);
+  const { flightId } = req.params;
+  try {
+    const foundFlight = await Flight.findByPk(flightId);
+    if (foundFlight) {
+      await foundFlight.update(req.body);
 
-			res.status(204).end();
-		} else {
-			res.status(404).json({ message: 'Sorry Flight Not Found!' });
-		}
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "Sorry Flight Not Found!" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
