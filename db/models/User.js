7f91e5cfcecb9ab24ba55e0ec4bdd92a014e1886
Mahmood28 <mahmoodalwatani@gmail.com>
2021-03-15 13:01:04 +0300

    clean up

--- db/models/User.js
+++ db/models/User.js
@@ -1,49 +1,49 @@
 module.exports = (sequelize, DataTypes) => {
-	const User = sequelize.define(
-		'User',
-		{
-			username: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				unique: {
-					args: true,
-					msg: 'Username already exists',
-				},
-			},
-			password: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			email: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				unique: {
-					args: true,
-					msg: 'Username already exists',
-				},
-				//   validate: {
-				//     isEmail: true
-				//   }
-			},
-			firstName: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			lastName: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			passport: {
-				type: DataTypes.STRING,
-				allowNull: true,
-			},
-			picture: {
-				type: DataTypes.STRING,
-				allowNull: true,
-			},
-		},
-		{ timestamps: false }
-	);
+  const User = sequelize.define(
+    "User",
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          args: true,
+          msg: "Username already exists",
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          args: true,
+          msg: "Email already exists",
+        },
+        //   validate: {
+        //     isEmail: true
+        //   }
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      passport: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      picture: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    { timestamps: false }
+  );
 
-	return User;
+  return User;
 };
